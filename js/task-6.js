6f370cbf67cd28e0bf4f81d9677aed62fea6edaa
wowka130299 <wolodya700@icloud.com>
2024-03-12 20:32:48 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,9 +1,10 @@
- function getRandomHexColor() {
+function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
   }
 
   function createBoxes(amount) {
     const boxesContainer = document.getElementById('boxes');
+    boxesContainer.innerHTML = '';
 
     for (let i = 0; i < amount; i++) {
       const box = document.createElement('div');
@@ -31,6 +32,8 @@
       if (amount >= 1 && amount <= 100) {
         createBoxes(amount);
         input.value = '';
+      } else {
+        alert('Please enter a number between 1 and 100.');
       }
     });
 
